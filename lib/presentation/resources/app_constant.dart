0daaae2cd8e76a201d5da617593932e1adf456b5
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-12-24 21:22:31 +0700

    rename

---
+++ lib/presentation/resources/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant{
+  static const String CROP_SEHAT = "sehat";
+  static const String CROP_SAKIT = "sakit";
+  static const String CROP_MATI= "mati";
+  static const String CROP_DIPANEN = "dipanen";
+}
