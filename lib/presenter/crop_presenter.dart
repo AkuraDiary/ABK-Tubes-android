5038af1f661f8ab03f3003cc4725f590a2a45740
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-12-26 16:59:56 +0700

    crop presenter

---
+++ lib/presenter/crop_presenter.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:asisten_buku_kebun/data/model/crop_model.dart';
+import 'package:asisten_buku_kebun/data/request_state.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class CropPresenter {
+  RequestState requestState = RequestState.initial;
+
+  List<CropModel> myCrops = [];
+  String message = '';
+  CropModel? selectedCrop;
+
+  void reset() {
+    requestState = RequestState.initial;
+    myCrops = [];
+    message = '';
+    selectedCrop = null;
+  }
+
+  Future<void> fetchMyCrops(String userId) async {
+    requestState = RequestState.initial;
+    try {
+      requestState = RequestState.loading;
+      var result = Supabase.instance.client
+          .from('crops')
+          .select()
+          .eq('user_id', userId);
+      var cropsData = await result.then((value) => value as List<dynamic>);
+      myCrops = cropsData.map((e) => CropModel.fromJson(e)).toList();
+      requestState = RequestState.success;
+    } catch (e) {
+      requestState = RequestState.error;
+      message = e.toString();
+    }
+  }
+
+  Future<void> addCrop(CropModel crop) async {
+    requestState = RequestState.initial;
+    try {
+      requestState = RequestState.loading;
+      await Supabase.instance.client.from('crops').insert(crop.toJson());
+      requestState = RequestState.success;
+    } catch (e) {
+      requestState = RequestState.error;
+      message = e.toString();
+    }
+  }
+
+  Future<void> editCrop(CropModel crop) async {
+    requestState = RequestState.initial;
+    try {
+      requestState = RequestState.loading;
+      await Supabase.instance.client
+          .from('crops')
+          .update(crop.toJson())
+          .eq('crop_id', crop.cropId!);
+      requestState = RequestState.success;
+    } catch (e) {
+      requestState = RequestState.error;
+      message = e.toString();
+    }
+  }
+
+}
