cd93e648d7455ffef3381891312cd32c1c70ade7
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2025-12-27 10:52:45 +0700

    my crops screen

--- lib/presentation/screens/crop/my_crops_screen.dart
+++ lib/presentation/screens/crop/my_crops_screen.dart
@@ -1,7 +1,9 @@
 import 'package:asisten_buku_kebun/DI.dart';
+import 'package:asisten_buku_kebun/data/model/crop_model.dart';
 import 'package:asisten_buku_kebun/data/request_state.dart';
 import 'package:asisten_buku_kebun/presentation/common/util/app_formatters.dart';
 import 'package:asisten_buku_kebun/presentation/common/util/app_toast.dart';
+import 'package:asisten_buku_kebun/presentation/resources/app_colors.dart';
 import 'package:asisten_buku_kebun/presentation/resources/app_constant.dart';
 import 'package:asisten_buku_kebun/presentation/routing/app_routes.dart';
 import 'package:asisten_buku_kebun/presentation/routing/app_routing.dart';
@@ -31,42 +33,28 @@ class _MyCropsScreenState extends State<MyCropsScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('My Crops')),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primary900,
+        foregroundColor: AppColors.white,
         onPressed: () {
           // Navigate to add crop screen
           AppRouting().navigateTo(AppRoutes.addEditCropScreen);
         },
+        child: const Icon(Icons.add),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              if (widget.cropPresenter.requestState == RequestState.loading)
-                const Center(child: CircularProgressIndicator()),
-              if (widget.cropPresenter.requestState == RequestState.success)
-                if (widget.cropPresenter.myCrops.isEmpty)
-                  const Center(child: Text('No crops found.'))
-                else
-                  ...widget.cropPresenter.myCrops.map(
-                    (crop) => InkWell(
-                      onTap: () {
-                        widget.cropPresenter.selectedCrop = crop;
-                        AppRouting().navigateTo(AppRoutes.detailCropScreen);
-                      },
-                      child: ListTile(
-                        title: Text(crop.cropName ?? ""),
-                        leading: Badge(
-                          label: Text(crop.cropStatus ?? ""),
-                          backgroundColor: _getCropStatusColor(crop.cropStatus?.toLowerCase()),
-                        ),
-                        subtitle: Text(
-                          'Planted on: ${formatDisplayDate(crop.createdAt)}',
-                        ),
-                      ),
-                    ),
-                  ),
-            ],
-          ),
+      body: SingleChildScrollView(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            if (widget.cropPresenter.requestState == RequestState.loading)
+              const Center(child: CircularProgressIndicator()),
+            if (widget.cropPresenter.requestState == RequestState.success)
+              if (widget.cropPresenter.myCrops.isEmpty)
+                const Center(child: Text('No crops found.'))
+              else
+                ...widget.cropPresenter.myCrops.map(
+                  (crop) => _buildCropItem(crop),
+                ),
+          ],
         ),
       ),
     );
@@ -118,5 +106,38 @@ class _MyCropsScreenState extends State<MyCropsScreen> {
       default:
         return Colors.grey;
     }
+  }
+
+  Widget _buildCropItem(CropModel crop) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      decoration: BoxDecoration(
+        color: AppColors.primary900,
+        border: Border.all(color: Colors.grey.shade300),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: ListTile(
+        title: Text(
+          crop.cropName ?? "",
+          style: TextStyle(color: AppColors.white),
+        ),
+        onTap: () {
+          widget.cropPresenter.selectedCrop = crop;
+          AppRouting().navigateTo(AppRoutes.detailCropScreen);
+        },
+        leading: Badge(
+          padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+          label: Text(
+            crop.cropStatus ?? "",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: _getCropStatusColor(crop.cropStatus?.toLowerCase()),
+        ),
+        subtitle: Text(
+          'Planted on: ${formatDisplayDate(crop.createdAt)}',
+          style: TextStyle(color: AppColors.white),
+        ),
+      ),
+    );
   }
 }
